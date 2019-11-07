25badcf22688d665fb8347a77f417e32ce1004dc
arpitgpta <55844150+arpitgpta@users.noreply.github.com>
2019-11-07 17:04:18 +0530

    Update Classes.cpp

--- Classes.cpp
+++ Classes.cpp
@@ -1,16 +1,14 @@
 
 #include "Structures.cpp"
 
-
 class Train {
 
     private:
 
         seat* create_seat (int s, int e) {
             seat *st;
-            if(s == e){
-                st = new seat();
-            }
+            if(s == e)  st = new seat();
+            
             else{
                 seat* l = create_seat(s, (s+e)/2);
                 seat* r = create_seat((s+e)/2+1, e);
@@ -22,67 +20,56 @@ class Train {
             return st;
         }
 
-        bool is_avilable(int from, int to, seat *st, int k){
+        bool is_available(int from, int to, seat *st, int k){
 
-            // for(int i = 0; i < k; i++) cout << "\t";
-            // cout << "st->from " << st->from << " st->to " << st->to << " from " << from << " to " << to << endl;
             
-            if(st == NULL ){
-                // for(int i = 0; i < k; i++) cout << "\t";
-                // cout << "st == null, returning true" << endl;
-                return true;
-            } 
-            if(st->from == st->to){
-                // for(int i = 0; i < k; i++) cout << "\t";
-                // cout << "st->from == st->to returning " << st->avilable << endl;
-                return st->avilable;
-            } 
+            if(st == NULL )   return true; 
+
+            if(st->from == st->to)    return st->avilable;
             
-            else if(to >= st->to && from <= st->from){
-                // for(int i = 0; i < k; i++) cout << "\t";
-                // cout << "to >= st->to && from <= st->from, returning" << st->avilable << endl;
-                return st->avilable;
-            } 
-            else if(st->from > to || st->to < from){
-                // for(int i = 0; i < k; i++) cout << "\t";
-                // cout << "out of bound returning true" << endl; 
-                return true;
-            } 
+            else if(to >= st->to && from <= st->from)   return st->avilable;
+
+            else if(st->from > to || st->to < from)    return true;
+            
             else if(st->from <= from || st->to >= to){
-                bool a = is_avilable(from, to, st->lch, k+1);
-                bool b = is_avilable(from, to, st->rch, k+1);
-                // for(int i = 0; i < k; i++) cout << "\t";
-                // cout << "recursive case a = " << a << " b= " << b << " returning " << (a&b) << endl;
+                bool a = is_available(from, to, st->lch, k+1);
+                bool b = is_available(from, to, st->rch, k+1);
                 return (a&b);
             }   
+            
+            return true;
         }
 
     public:
 
-        Train(string name, string date, int number){
+        Train(){
+            
+        }
+
+        Train(string date, int no, string name){
             for(int i = 0; i < 40; i++){
                 seat *temp = create_seat(1, 16);
                 Seats.push_back(temp);
             }
             
             train_name = name;
-            train_jdate = date;
-            train_number = number;
+            train_number = no;
+            jdate = date;
 
         }
 
-        string train_jdate;
         string train_name;
         int train_number;
+        string jdate;
         vector<seat*> Seats;
 
-        int avilable_seats(int from, int to){
+        int available_seats(int from, int to){
             if(from > to) swap(from, to);
             int total_seats = 0;
             deque <int> avl_seat_nos;
             for(int i = 0; i < 40; i++){
                 // cout << "calling fun " << endl;
-                bool temp = is_avilable(from, to-1, Seats[i], 0);
+                bool temp = is_available(from, to-1, Seats[i], 0);
                 // cout << "temp " << temp << endl;
                 if(temp){
                     total_seats++;
@@ -92,4 +79,12 @@ class Train {
             }
             return total_seats;
         }
+
+        ticket* book(int psngrs, vector<psgr*> psgr_list){
+            // this function will book ticket and returns 1.pnr number 2.total ammount 3.list of alloted seats  
+            ticket* tkt = new ticket();
+
+            return tkt;
+        }
+
 };
