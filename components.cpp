c044203765cb8a081a2cd192cdddfa471bfafe8d
arpitgpta <55844150+arpitgpta@users.noreply.github.com>
2019-11-10 01:05:11 +0530

    Create components.cpp

---
+++ components.cpp
@@ -0,0 +1,144 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+#include <fstream>
+
+ofstream fout;
+ifstream fin;
+
+struct station{
+
+    char st_name[25];
+    int st_no;
+    int km;
+
+};
+
+struct all_stations{
+    station Stations[100];
+};
+
+all_stations STATIONS;
+
+
+
+
+struct train{
+
+    char jdate[15];
+    char train_name[35];
+    int train_number;
+    int seat[40][32][3];    
+
+};
+
+struct all_trains{
+
+
+    train DtoP[60];
+    train PtoD[60];
+
+};
+
+all_trains TRAINS;
+
+
+struct psgr{
+
+    char name[30];
+    int age;
+    char gender;
+    int seat;
+
+};
+
+struct ticket{
+
+    int from, to;
+    int psgr_count;
+    psgr psgrs[15];
+    int pnr;
+
+    char jdate[15];
+    char tr_start_DorP;
+    int train_no;
+
+    int amount;
+
+};
+
+struct all_ticket{
+    ticket tickets[500];
+    int ticket_counter;
+};
+
+all_ticket TICKETS;
+
+
+// functions
+
+void cancel_ticket(int pnr){
+
+}
+
+bool is_available(int from, int to, char start_st, int trno_inlist, int seat_no, int ctr_in){
+
+
+    return true;
+}
+
+int available_seats(int from, int to, char start_st, int trno_inlist){
+
+    int total = 0;
+    for(int i = 0; i < 40; i++) if(is_available(from, to, start_st, trno_inlist, i, 1)) total++;
+    return total;
+}
+
+int book_ticket(){
+
+
+    return 0;
+}
+
+
+void re_read(){
+
+    fin.open("Trains.dat");
+    fin.read((char*)&TRAINS, sizeof(TRAINS));
+    fin.close();
+
+    fin.open("tickets.dat");
+    fin.read((char*)&TICKETS, sizeof(TICKETS));
+    fin.close();    
+
+}
+
+
+
+void re_write(){
+
+    fout.open("Trains.dat");
+    fout.write((char*)&TRAINS, sizeof(TRAINS));
+    fout.close();
+
+    fout.open("tickets.dat");
+    fout.write((char*)&TICKETS, sizeof(TICKETS));
+    fout.close();
+
+}
+
+void load_data(){
+
+    fin.open("Stations.dat");
+    fin.read((char*)&STATIONS, sizeof(STATIONS));
+    fin.close();
+    
+    fin.open("Trains.dat");
+    fin.read((char*)&TRAINS, sizeof(TRAINS));
+    fin.close();
+
+    fin.open("tickets.dat");
+    fin.read((char*)&TICKETS, sizeof(TICKETS));
+    fin.close();
+
+}
