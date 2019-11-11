8c8b2e5d20e9a2d9abd8e03481b1be831b9ba968
arpitgpta <55844150+arpitgpta@users.noreply.github.com>
2019-11-12 02:06:40 +0530

    Update components.cpp

--- components.cpp
+++ components.cpp
@@ -78,35 +78,85 @@ all_ticket TICKETS;
 // functions
 
 void cancel_ticket(int pnr){
-            
-    // 137
-    
+
+    // 136, 137
+
+    // remove from ticket list
+    // unmark in train seat segment tree
+
+
 }
 
-bool is_available(int from, int to, char start_st, int trno_inlist, int seat_no, int ctr_in){
-
-    // 140
+int is_available(ticket tkt, int trno_inlist, int seat_no, int ctr_index){
     
-    return true;
+    // done -- 140
+
+    if(tkt.tr_start_DorP == 'd'){
+        if(TRAINS.DtoP[trno_inlist].seat[seat_no][ctr_index][0] == TRAINS.DtoP[trno_inlist].seat[seat_no][ctr_index][1]){
+            return TRAINS.DtoP[trno_inlist].seat[seat_no][ctr_index][2];
+        }
+        else if(tkt.to >= TRAINS.DtoP[trno_inlist].seat[seat_no][ctr_index][1] && tkt.from <= TRAINS.DtoP[trno_inlist].seat[seat_no][ctr_index][0]){
+            return TRAINS.DtoP[trno_inlist].seat[seat_no][ctr_index][2];
+        }
+        else if(TRAINS.DtoP[trno_inlist].seat[seat_no][ctr_index][0] > tkt.to || TRAINS.DtoP[trno_inlist].seat[seat_no][ctr_index][1] < tkt.from) return 1;
+        else if(TRAINS.DtoP[trno_inlist].seat[seat_no][ctr_index][0] <= tkt.from || TRAINS.DtoP[trno_inlist].seat[seat_no][ctr_index][1] >= tkt.to){
+            int a = is_available(tkt, trno_inlist, seat_no, ctr_index*2);
+            int b = is_available(tkt, trno_inlist, seat_no, ctr_index*2+1);
+            return a&&b;
+        }
+    }
+    else{
+        if(TRAINS.PtoD[trno_inlist].seat[seat_no][ctr_index][0] == TRAINS.PtoD[trno_inlist].seat[seat_no][ctr_index][1]){
+            return TRAINS.PtoD[trno_inlist].seat[seat_no][ctr_index][2];
+        }
+        else if(tkt.to >= TRAINS.PtoD[trno_inlist].seat[seat_no][ctr_index][1] && tkt.from <= TRAINS.PtoD[trno_inlist].seat[seat_no][ctr_index][0]){
+            return TRAINS.PtoD[trno_inlist].seat[seat_no][ctr_index][2];
+        }
+        else if(TRAINS.PtoD[trno_inlist].seat[seat_no][ctr_index][0] > tkt.to || TRAINS.PtoD[trno_inlist].seat[seat_no][ctr_index][1] < tkt.from) return 1;
+        else if(TRAINS.PtoD[trno_inlist].seat[seat_no][ctr_index][0] <= tkt.from || TRAINS.PtoD[trno_inlist].seat[seat_no][ctr_index][1] >= tkt.to){
+            int a = is_available(tkt, trno_inlist, seat_no, ctr_index*2);
+            int b = is_available(tkt, trno_inlist, seat_no, ctr_index*2+1);
+            return a&&b;
+        }
+    }    
+    return 1;
 }
 
-int available_seats(int from, int to, char start_st, int trno_inlist){
+pair<deque<int>, int> available_seats(ticket tkt, int trno_inlist){
 
-    // 140
-    
+    // done-- 140
+
+    deque<int> avl_seats;
+    int total = 0;
+    for(int i = 0; i < 40; i++) if(is_available(tkt, trno_inlist, i, 1)){
+        if(i%7 == 0 || i%7 == 3 || i%7 == 7) avl_seats.push_front(i);
+        total++;
+    } 
+    return make_pair(avl_seats, total);
 }
 
-int book_ticket(){
+int book_ticket(deque<int> avl_seats, int trno_inseq){
 
-    // 141, 137
-    
+    // 138, 141
+
+    // mark in train
+    // allot seat
+    // generate pnr
+    // calculate amount
+
     return 0;
 }
 
 
 void re_read(){
 
-    // 136
+    fin.open("Trains.dat");
+    fin.read((char*)&TRAINS, sizeof(TRAINS));
+    fin.close();
+
+    fin.open("tickets.dat");
+    fin.read((char*)&TICKETS, sizeof(TICKETS));
+    fin.close();    
 
 }
 
@@ -114,12 +164,28 @@ void re_read(){
 
 void re_write(){
 
-     // 136
-    
+    fout.open("Trains.dat");
+    fout.write((char*)&TRAINS, sizeof(TRAINS));
+    fout.close();
+
+    fout.open("tickets.dat");
+    fout.write((char*)&TICKETS, sizeof(TICKETS));
+    fout.close();
+
 }
 
 void load_data(){
+
+    fin.open("Stations.dat");
+    fin.read((char*)&STATIONS, sizeof(STATIONS));
+    fin.close();
     
-    // 136
-    
+    fin.open("Trains.dat");
+    fin.read((char*)&TRAINS, sizeof(TRAINS));
+    fin.close();
+
+    fin.open("tickets.dat");
+    fin.read((char*)&TICKETS, sizeof(TICKETS));
+    fin.close();
+
 }
